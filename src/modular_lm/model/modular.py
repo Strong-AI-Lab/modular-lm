dd4284632dcd457f5c5da02a2a60411f46fc1d32
ggendro <g.gendron35@gmail.com>
2023-12-22 06:25:09 +0000

    added `prepare_inputs_for_generation` function to modular for inference

--- src/modular_lm/model/modular.py
+++ src/modular_lm/model/modular.py
@@ -439,3 +439,44 @@ class ModularModel(PreTrainedModel):
                 model.domain_models[i] = PeftModel.from_pretrained(model.domain_models[i], domain_directories[i])
 
         return model
+    
+    
+
+    def prepare_inputs_for_generation( # Taken from <https://github.com/huggingface/transformers/blob/main/src/transformers/models/llama/modeling_llama.py#L1227>
+        self, input_ids, past_key_values=None, attention_mask=None, inputs_embeds=None, **kwargs
+    ):
+        if past_key_values is not None:
+            past_length = past_key_values[0][0].shape[2]
+
+            # Some generation methods already pass only the last input ID
+            if input_ids.shape[1] > past_length:
+                remove_prefix_length = past_length
+            else:
+                # Default to old behavior: keep only final ID
+                remove_prefix_length = input_ids.shape[1] - 1
+
+            input_ids = input_ids[:, remove_prefix_length:]
+
+        position_ids = kwargs.get("position_ids", None)
+        if attention_mask is not None and position_ids is None:
+            # create position_ids on the fly for batch generation
+            position_ids = attention_mask.long().cumsum(-1) - 1
+            position_ids.masked_fill_(attention_mask == 0, 1)
+            if past_key_values:
+                position_ids = position_ids[:, -input_ids.shape[1] :]
+
+        # if `inputs_embeds` are passed, we only want to use them in the 1st generation step
+        if inputs_embeds is not None and past_key_values is None:
+            model_inputs = {"inputs_embeds": inputs_embeds}
+        else:
+            model_inputs = {"input_ids": input_ids}
+
+        model_inputs.update(
+            {
+                "position_ids": position_ids,
+                "past_key_values": past_key_values,
+                "use_cache": kwargs.get("use_cache"),
+                "attention_mask": attention_mask,
+            }
+        )
+        return model_inputs
