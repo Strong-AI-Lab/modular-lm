d003e0cc66f9709a3ff03311d762a09be4f272a9
ggendro <g.gendron35@gmail.com>
2024-02-01 08:08:04 +0000

    added loading option for routing strategy with higher number of modules

--- src/modular_lm/router/loader.py
+++ src/modular_lm/router/loader.py
@@ -18,12 +18,13 @@ ROUTERS = {
     "InputReductionQuantizer": InputReductionQuantizer
 }
 
-def load_router(router_name : str, router_config : dict, router_path : Optional[str] = None, allow_different_centroid_number : bool = False):
+def load_router(router_name : str, router_config : dict, router_path : Optional[str] = None):
     if router_name not in ROUTERS:
         raise ValueError(f"Unknown router name: {router_name}")
     
     routing_strategy = ROUTERS[router_name](**router_config)
     if router_path is not None:
+        allow_different_centroid_number = "allow_different_centroid_number" in router_config and router_config["allow_different_centroid_number"]
         routing_strategy.load_strategy(router_path, allow_different_centroid_number=allow_different_centroid_number)
 
     return routing_strategy
